31e6f09a3e85a58ca8e805d1909ef36236baac19
Michael Vorburger <mike@vorburger.ch>
2010-10-13 22:43:17 +0200

    Need to wait for mySQL start-up before running mifos SQL..

--- bin/mac/init-ramdisk.sh
+++ bin/mac/init-ramdisk.sh
@@ -2,5 +2,6 @@ diskutil erasevolume HFS+ "ramdisk" `hdiutil attach -nomount ram://976998`
 mkdir /Volumes/ramdisk/mysqldata
 /usr/local/mysql-5.1.51-osx10.6-x86/scripts/mysql_install_db --basedir=/usr/local/mysql-5.1.51-osx10.6-x86 --datadir /Volumes/ramdisk/mysqldata
 /usr/local/mysql-5.1.51-osx10.6-x86/bin/mysqld_safe &
+sleep 2
 ./create-mifos.sh
 
