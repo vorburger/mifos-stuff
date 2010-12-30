36752df8c96b6a8a772d8ce56bd0e7693a1ecab1
Michael Vorburger <mike@vorburger.ch>
2010-12-30 17:23:24 +0100

    mifostest should also be initialized

--- bin/create-mifos.sh
+++ bin/create-mifos.sh
@@ -1,4 +1,7 @@
 /usr/local/mysql/bin/mysql -u root < create-mifos.sql
-/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/latest-schema.sql
-/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/latest-data.sql
-/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/init_mifos_password.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < ../../vorburger-mifos-head/application/src/main/sql/latest-schema.sql
+/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/latest-schema.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < ../../vorburger-mifos-head/application/src/main/sql/latest-data.sql
+/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/latest-data.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < ../../vorburger-mifos-head/application/src/main/sql/init_mifos_password.sql
+/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/init_mifos_password.sql
