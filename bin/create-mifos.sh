885450c267ed3b7c14930a97b96ebb7b27289d1c
Michael Vorburger <mike@vorburger.ch>
2010-12-30 15:02:00 +0100

    Relative paths

--- bin/create-mifos.sh
+++ bin/create-mifos.sh
@@ -1,4 +1,4 @@
-mysql -u root < create-mifos.sql
-mysql -D mifos -u mifos -pmifos < ../../mifos-head/application/src/main/sql/latest-schema.sql
-mysql -D mifos -u mifos -pmifos < ../../mifos-head/application/src/main/sql/latest-data.sql
-mysql -D mifos -u mifos -pmifos < ../../mifos-head/application/src/main/sql/init_mifos_password.sql
+/usr/local/mysql/bin/mysql -u root < create-mifos.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/latest-schema.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/latest-data.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/init_mifos_password.sql
