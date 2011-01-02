26368373734abfe53206632a158f522f1059c686
Michael Vorburger <mike@vorburger.ch>
2011-01-02 19:53:28 +0100

    Fixing clearly wrong Mifos Unicode configuration (see mailing list), and adoption to new sql directory

--- bin/create-mifos.sh
+++ bin/create-mifos.sh
@@ -1,10 +1,11 @@
+SQL_DIR=../../vorburger-mifos-head/application/src/main/resources/sql
 /usr/local/mysql/bin/mysql -u root < create-mifos.sql
-/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < ../../vorburger-mifos-head/application/src/main/sql/latest-schema.sql
-/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/latest-schema.sql
-/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < ../../vorburger-mifos-head/application/src/main/sql/latest-data.sql
-/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/latest-data.sql
-/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < ../../vorburger-mifos-head/application/src/main/sql/custom_data.sql
-/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/custom_data.sql
-/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < ../../vorburger-mifos-head/application/src/main/sql/init_mifos_password.sql
-/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/init_mifos_password.sql
-/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < ../../vorburger-mifos-head/application/src/main/sql/testdbinsertionscript.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < $SQL_DIR/latest-schema.sql
+/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < $SQL_DIR/latest-schema.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < $SQL_DIR/latest-data.sql
+/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < $SQL_DIR/latest-data.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < $SQL_DIR/custom_data.sql
+/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < $SQL_DIR/custom_data.sql
+/usr/local/mysql/bin/mysql -D mifos -u mifos -pmifos     < $SQL_DIR/init_mifos_password.sql
+/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < $SQL_DIR/init_mifos_password.sql
+/usr/local/mysql/bin/mysql -D mifostest -u mifos -pmifos < $SQL_DIR/testdbinsertionscript.sql
