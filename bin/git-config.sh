e58cfa84fc6467d6fd5716d43b23e3b2f02ae1b8
Michael Vorburger <mike@vorburger.ch>
2011-01-02 01:53:39 +0100

    git config ...

---
+++ bin/git-config.sh
@@ -0,0 +1,11 @@
+git config --global user.name "Michael Vorburger"
+git config --global user.email mike@vorburger.ch
+git config branch.autosetupmerge true
+git config --global apply.whitespace nowarn
+git config --global diff.renames true
+# On Linux/Mac:
+git config --global core.autocrlf input
+## On Windows:
+### git config --global core.autocrlf true
+git config --global color.diff true
+git config --global color.status true
