d52df2005ecc6b4245a17384b2cf0e33d76bce77
Michael Vorburger <mike@vorburger.ch>
2011-02-05 02:10:00 +0100

    Adapt to new Mifos SQL scripts

--- bin/create-mifos.sh
+++ bin/create-mifos.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-SQL_DIR=../../vorburger-mifos-head/application/src/main/resources/sql
+SQL_DIR=../../vorburger-mifos-head/db/src
+MAIN_SQL_DIR=$SQL_DIR/main/resources/sql
+TEST_SQL_DIR=$SQL_DIR/test/resources/sql
 
 mysql -u root < create-mifos.sql
-mysql -D mifos -u mifos -pmifos     < $SQL_DIR/latest-schema.sql
-mysql -D mifostest -u mifos -pmifos < $SQL_DIR/latest-schema.sql
-mysql -D mifos -u mifos -pmifos     < $SQL_DIR/latest-data.sql
-mysql -D mifostest -u mifos -pmifos < $SQL_DIR/latest-data.sql
-mysql -D mifos -u mifos -pmifos     < $SQL_DIR/custom_data.sql
-mysql -D mifostest -u mifos -pmifos < $SQL_DIR/custom_data.sql
-mysql -D mifos -u mifos -pmifos     < $SQL_DIR/init_mifos_password.sql
-mysql -D mifostest -u mifos -pmifos < $SQL_DIR/init_mifos_password.sql
-mysql -D mifostest -u mifos -pmifos < $SQL_DIR/testdbinsertionscript.sql
+mysql -D mifos -u mifos -pmifos     < $MAIN_SQL_DIR/base-schema.sql
+mysql -D mifostest -u mifos -pmifos < $MAIN_SQL_DIR/base-schema.sql
+mysql -D mifos -u mifos -pmifos     < $MAIN_SQL_DIR/base-data.sql
+mysql -D mifostest -u mifos -pmifos < $MAIN_SQL_DIR/base-data.sql
+mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/integration_test_data.sql
+mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/integration_test_data.sql
+mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/acceptance_test_data.sql
+mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/acceptance_test_data.sql
+mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/init_mifos_password.sql
+mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/init_mifos_password.sql
