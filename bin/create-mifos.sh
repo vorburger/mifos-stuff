fb4387b62458bd76716ce21ceb311ea8112cac0f
Michael Vorburger <mike@vorburger.ch>
2011-02-15 20:26:15 +0100

    MIFOS-4637

--- bin/create-mifos.sh
+++ bin/create-mifos.sh
@@ -9,8 +9,9 @@ mysql -D mifos -u mifos -pmifos     < $MAIN_SQL_DIR/base-schema.sql
 mysql -D mifostest -u mifos -pmifos < $MAIN_SQL_DIR/base-schema.sql
 mysql -D mifos -u mifos -pmifos     < $MAIN_SQL_DIR/base-data.sql
 mysql -D mifostest -u mifos -pmifos < $MAIN_SQL_DIR/base-data.sql
-mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/integration_test_data.sql
-mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/integration_test_data.sql
+# http://mifosforge.jira.com/browse/MIFOS-4637 says you must use either integration_* or acceptance_*, so: 
+# mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/integration_test_data.sql
+# mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/integration_test_data.sql
 mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/acceptance_test_data.sql
 mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/acceptance_test_data.sql
 mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/init_mifos_password.sql
