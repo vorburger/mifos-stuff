664017bab7a18d0776e9faa9fe8e39e0afbd633f
Michael Vorburger <mike@vorburger.ch>
2011-02-17 22:52:18 +0100

    1. New Local Dir,  2. init_mifos_password moved

--- bin/create-mifos.sh
+++ bin/create-mifos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SQL_DIR=../../vorburger-mifos-head/db/src
+SQL_DIR=../../head/db/src
 MAIN_SQL_DIR=$SQL_DIR/main/resources/sql
 TEST_SQL_DIR=$SQL_DIR/test/resources/sql
 
@@ -14,5 +14,5 @@ mysql -D mifostest -u mifos -pmifos < $MAIN_SQL_DIR/base-data.sql
 # mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/integration_test_data.sql
 mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/acceptance_test_data.sql
 mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/acceptance_test_data.sql
-mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/init_mifos_password.sql
-mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/init_mifos_password.sql
+mysql -D mifos -u mifos -pmifos     < $MAIN_SQL_DIR/init_mifos_password.sql
+mysql -D mifostest -u mifos -pmifos < $MAIN_SQL_DIR/init_mifos_password.sql
