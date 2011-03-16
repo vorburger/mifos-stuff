1159a878bf260bbf14ef5266203e0ae09dbd2d9f
Michael Vorburger <mike@vorburger.ch>
2011-03-17 00:14:38 +0100

    Somebody change the filename of an SQL script again... ;)

--- bin/create-mifos.sh
+++ bin/create-mifos.sh
@@ -12,7 +12,7 @@ mysql -D mifostest -u mifos -pmifos < $MAIN_SQL_DIR/base-data.sql
 # http://mifosforge.jira.com/browse/MIFOS-4637 says you must use either integration_* or acceptance_*, so: 
 # mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/integration_test_data.sql
 # mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/integration_test_data.sql
-mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/acceptance_test_data.sql
-mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/acceptance_test_data.sql
+mysql -D mifos -u mifos -pmifos     < $TEST_SQL_DIR/acceptance_test_dump.sql
+mysql -D mifostest -u mifos -pmifos < $TEST_SQL_DIR/acceptance_test_dump.sql
 mysql -D mifos -u mifos -pmifos     < $MAIN_SQL_DIR/init_mifos_password.sql
 mysql -D mifostest -u mifos -pmifos < $MAIN_SQL_DIR/init_mifos_password.sql
